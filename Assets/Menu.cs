82de1f851c7a78d3223e95a5f4aa4e967bb65f69
Sabarish2003 <gnasa@utschools.ca>
2018-05-20 23:07:56 -0400

    Initial commit

---
+++ Assets/Menu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu : MonoBehaviour
+{
+    public GameObject altspacevr;
+    public GameObject mergevr;
+    public GameObject quantumCapture;
+    public GameObject neurable;
+    public GameObject decentraland;
+
+    public GameObject canvas;
+
+    public void Click()
+    {
+        altspacevr.GetComponent<AudioSource>().Play();
+        mergevr.GetComponent<AudioSource>().Play();
+        quantumCapture.GetComponent<AudioSource>().Play();
+        neurable.GetComponent<AudioSource>().Play();
+        decentraland.GetComponent<AudioSource>().Play();
+        canvas.SetActive(false);
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+Unity 2017.1.0p4
+Google VR SDK 1.120
+https://medium.com/@sabarish.gnanamoorthy/vr-platforms-and-applications-vr-innovation-gallery-da9b47c2d12e
